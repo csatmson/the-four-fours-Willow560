4a6463d980a18c8a2d5a0f77fb4d221219992a6b
Willow560 <Willow560@users.noreply.github.com>
2019-09-17 10:54:17 -0400

    Update 4fours.py

--- 4fours.py
+++ 4fours.py
@@ -5,10 +5,11 @@
 print("Zero is ", 4+4-4-4)
 print("One is ", (4+4)/(4+4))
 print("Two is ", (4/4)+(4/4))
-print("Three is ", 
-print("Four is ", (4 ** (4-4)
-print("Five is ", ((4 ** (4-4)) + 4)                   
+print("Three is ", ((4 * 4) - 4) / 4)
+print("Four is ", (4 + (4 * (4-4))))
+print("Five is ", (4 ** (4-4)) + 4)
+print("Six is ", (((4+4) / 4) + 4))
 print("Seven is ", (44/4) - 4)
-print("Eight is ", (4+4) + (4-4)) 
-print("Nine is ",    
+print("Eight is ", (4+4) + (4-4))
+print("Nine is ", (4/4) + (4+4))
 print("Ten is ", (44-4) / 4)
